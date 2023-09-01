4273438435058cd1edf7d19d988565bb883d9370
okuzmychov <o.kuzmichev@gmail.com>
2023-09-01 21:56:55 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { GlobalStyle } from 'GlobalStyle';
 import { Layout } from 'Layout';
 import { Component } from 'react';
+
 import { Contacts } from './Contacts/Contacts';
 import { nanoid } from 'nanoid';
 
@@ -8,32 +9,27 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
-    number: '',
   };
 
-  render() {
-    const { contacts, name, number, searchContact } = this.state;
+  addContact = newContact => {
+    this.setState(pState => ({
+      contacts: [...pState.contacts, newContact],
+    }))
+  }
 
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(searchContact.toLowerCase())
-    );
+
+  render() {
+    const { contacts, name } = this.state;
 
     return (
       <Layout>
         <h1>Phonebook</h1>
-        <InputName
-          name={name}
-          number={number}
-          onChangeName={this.handleNameChange}
-          onChangeNumber={this.handleNumberChange}
-          onAddContact={this.handleAddContact}
-        />
-        <SearchField value={searchContact} onChange={this.handleSearchChange} />
+        <Contacts addContact={this.addContact}/>
+
         <h1>Contacts</h1>
-        <Contacts
-          contactInfo={{ contacts: filteredContacts }}
-          onDeleteContact={this.handleDeleteContact}
-        />
+        <Filter />
+        <ContactsList contacts={contacts}/>
+        
         <GlobalStyle />
       </Layout>
     );
