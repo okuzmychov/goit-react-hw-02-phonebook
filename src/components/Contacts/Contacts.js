191d8e166a919c0aff4f70802ed95f71cad8bb2e
okuzmychov <o.kuzmichev@gmail.com>
2023-09-02 13:22:09 +0300

    contact

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,5 +1,15 @@
-import { Wrapper, ContactInfo, ButtonDelete } from './Contacts.styled';
- import { Formik, Field, Form } from 'formik';
+import { Wrapper, Label, Button } from './Contacts.styled';
+import { Formik, Field } from 'formik';
+ import * as Yup from 'yup';
+
+ const ContactSchema = Yup.object().shape({
+   firstName: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('Required'),
+   number: Yup.string().email('Invalid email').required('Required'),
+ });
+ 
 
 export const Contacts = () => {
   return (
@@ -8,23 +18,24 @@ export const Contacts = () => {
       initialValues={{
         name: '',
         number: '',
-      }}
-      onAddcontact={values => {
+        }}
+        validationSchema={ContactSchema}
+        onAddcontact={values => {
         console.log(values);
       }}
     >
         <Wrapper>
-          <ContactInfo>
+          <Label>
             Name
             <Field name="name" type="text" placeholder="Oleg" />
-          </ContactInfo>
+          </Label>
 
-          <ContactInfo>
+          <Label>
             Number
-            <Field name="number" type="number" placeholder="123-45-67" />
-          </ContactInfo>
+            <Field name="number" type="tel" placeholder="123-45-67" />
+          </Label>
           
-        <button type="add-contact">Add contact</button>
+        <Button type="add-contact">Add contact</Button>
       </Wrapper>
     </Formik>
     </div>
--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -4,25 +4,29 @@ import { Form } from 'formik';
 export const Wrapper = styled(Form)`
   display: flex;
   flex-direction: column;
-  gap: 15px;
+  gap: 10px;
   padding: 12px;
   border-radius: 8px;
-  width: 350px;
+  width: 300px;
 `;
 
-export const ContactInfo = styled(Form)`
+export const Label = styled.label`
   display: flex;
-  flex-direction: row;
-  gap: 10px;
-  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  gap: 5px;
+  align-items: flex-start;
   justify-content: space-between;
 `;
 
-export const ButtonDelete = styled.button`
-  width: 80px;
+export const Button = styled(Form)`
+  display: flex;
+  width: 100%;
   padding: 4px;
-  border: none;
-  border-radius: 5px;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 3px;
+  justify-content: center;
   background-color: ${p => p.theme.colors.lightgreen};
   cursor: pointer;
   &:focus,
